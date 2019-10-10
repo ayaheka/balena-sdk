d4e99e59f19cca8948073285e067e6bd7bdacebb
Thodoris Greasidis <thgreasi@gmail.com>
2019-10-10 17:01:35 +0300

    Support creating applications for a specific organization
    Change-type: minor
    Depends-on: https://github.com/balena-io/balena-api/pull/2279
    Depends-on: https://github.com/balena-io/balena-api/pull/2281
    See: https://github.com/balena-io/balena-api/pull/2018
    Signed-off-by: Thodoris Greasidis <thodoris@balena.io>

--- typings/balena-sdk.d.ts
+++ typings/balena-sdk.d.ts
@@ -870,6 +870,7 @@ declare namespace BalenaSdk {
 					applicationType?: string;
 					deviceType: string;
 					parent?: number | string;
+					organization?: number | string;
 				}): Promise<Application>;
 				get(
 					nameOrId: string | number,
